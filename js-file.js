cf114793c37c295fa3f47b3f54fc176004282c1d
annalast1 <166384111+annalast1@users.noreply.github.comm>
2025-01-08 13:10:11 +0000

    JS regex removed previous char introduced

--- js-file.js
+++ js-file.js
@@ -1,74 +1,38 @@
-// Update display to show answer
-
+// Variables for each part of sum
+let num1 = null, num2 = null, operator = [], result = 0;
 // Basic calculator functions +,-,*,/
 function add(num1, num2) {
     return num1 + num2;
+    
 };
-
 function subtract(num1, num2) {
     return num1 - num2;
+   
 };
-
 function multiply(num1, num2) {
     return num1 * num2;
-}
-
-function divideIt(num1, num2) {
-    return num1 / num2;
-}
-
-// Variables for calculator operations
-let num1 = null, num2 = null, operator = null, displayValue = '';
-let display = document.getElementById("display");
-const regex = /[+|\-|x]/g;
-const userIn = display.textContent;
-const equal = document.getElementById("equal");
-equal.addEventListener("click", calculation);
-
-// Make buttons functional
-const digitButtons = document.querySelectorAll('.digit');
-digitButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        if (display.textContent === '0'){
-            display.textContent = ''; 
-            display.textContent += button.innerText;
-            displayValue += button.innerText;
+    
+};
+function divide(num1, num2) {
+    result = (num1 / num2)
+    if (Number.isInteger(result)) {
+        return result;
     } else {
-        display.textContent += button.innerText;
-        displayValue += button.innerText;
-        console.log(...displayValue);
-}})});
+        return Number(result.toFixed(6));
+    }
+};
 
-const opButton = document.querySelectorAll('.operator');
-opButton.forEach(button => {
-    button.addEventListener("click", () => {
-        if (displayValue.match(regex) != null) {
-            return;
-        }
-        else {
-            num1 = Number(displayValue);
-            console.log(typeof num1);
-            display.textContent += button.innerText;            
-            displayValue = button.innerText;
-
-        }    
-})});
-
-const clear = document.getElementById("clear");
-clear.addEventListener("click", () => {
-    display.textContent = '0';
-});
-
+// Call the appropriate calculator function
 function operate(num1, num2, operator) {
     switch (operator) {
         case '+':
             return add(num1, num2);
             break;
         case '-':
-            return minus(num1, num2);
+            return subtract(num1, num2);
             break;
         case 'x':
-            return times(num1, num2);
+            return multiply(num1, num2);
             break;
         case '/':
             return divide(num1, num2);
@@ -76,21 +40,73 @@ function operate(num1, num2, operator) {
         default:
             return "Operator Error";
         }
-    }
-       
-    function calculation() {
-    
-    // Create an array of all operators 
-    //let operator = [userIn.match(regex)];
-    //console.log(operator);
-    let op1 = userIn.search(regex);
-    
+};
+
+// DOM manipulation
+let display = document.getElementById("display");
+// Store input as string
+let userIn = '';
+// Store current value
+let displayValue = '';
+// Store last char
+let prev = '';
+
+
+
+const digitButtons = document.querySelectorAll('.digit');
+digitButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        // At the beginning of the sum overwrite the display of 0
+        if (display.textContent === '0'){
+            display.textContent = ''; 
+            display.textContent = button.innerText;
+            displayValue = button.innerText;
+            userIn = display.textContent;
+            prev = userIn.slice(-1);
+        } // if not first input just add the value to existing  
+            else {
+            display.textContent += button.innerText;
+            displayValue += button.innerText;
+            userIn = display.textContent;
+            prev = userIn.slice(-1);
+}})});
+
+// Make operator buttons functional
+const opButton = document.querySelectorAll('.operator');
+opButton.forEach(button => {
+    button.addEventListener("click", () => {  
+        // Stops repeated input of operators 
+        if (['+','-','x','/'].includes(prev)) {
+            return alert("too many operators");
+        } else {  
+            num1 = Number(displayValue);
+            displayValue = '';
+            display.textContent += button.innerText; 
+            userIn += button.innerText;  
+            prev = userIn.slice(-1);
+            operator += button.innerText;
+        } 
+})});
+
+// Make clear button functional
+const clear = document.getElementById("clear");
+clear.addEventListener("click", () => {
+    display.textContent = '0';
+    displayValue = '';
+    userIn = '';
+    operator = '';
+    num1 = null;
+    num2 = null;
+});
+
+//Make equal button functional
+const equal = document.getElementById("equal");
+equal.addEventListener("click", calculation);
+
+// Carry out sum
+function calculation() {
+    // Get digits input after operator 
     num2 = Number(displayValue);
     
-    console.log(num2); 
-    operate(num1, num2, op1);  
-    
-}  
-   calculation();
-   
-
+    console.log(operate(num1, num2, operator[0]));  
+} 
